a6d6367ee53ade5c4ed20b4601d1130ced2e9457
Mario <marietejr12@gmail.com>
2026-05-22 16:32:37 +0200

    Campos contabilidad

--- frontend/app/src/main/java/com/example/myapplication/model/MonthlyExpenseDto.kt
+++ frontend/app/src/main/java/com/example/myapplication/model/MonthlyExpenseDto.kt
@@ -1,8 +1,17 @@
 package com.example.myapplication.model
 
+import com.google.gson.annotations.SerializedName
+
 data class MonthlyExpenseDto(
+    @SerializedName("total")
     val total: Double,
+
+    @SerializedName("variationPercent")
     val variationPercent: Double,
+
+    @SerializedName("ticketsCount")
     val ticketsCount: Int,
+
+    @SerializedName("categoriesCount")
     val categoriesCount: Int
 )
