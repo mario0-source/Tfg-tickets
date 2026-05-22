502938db6ddb9eb0367cb39055c8fb74fc328ef6
Mario <marietejr12@gmail.com>
2026-05-22 16:21:46 +0200

    Dependencias OCR

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+    implementation("com.google.mlkit:text-recognition:16.0.0")
     implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.foundation.layout)
