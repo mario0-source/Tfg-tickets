183764e5000c30ad46856d5d81eb26f0069489af
Mario <marietejr12@gmail.com>
2026-05-22 16:33:27 +0200

    Pruebas para APK

--- frontend/app/src/main/java/com/example/myapplication/network/RetrofitClient.kt
+++ frontend/app/src/main/java/com/example/myapplication/network/RetrofitClient.kt
@@ -6,8 +6,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://10.0.2.2:8000/"
-
+    private const val BASE_URL = "http://192.168.1.11:8000/"
+    //10.0.2.2
     private var jwtToken: String? = null
 
     fun setToken(token: String?) {
